f126858b99ccf58493ac3faf7e37b34bda7dbedf
Hyuntae Kim <hyuntae.kim@swisscom.com>
2020-10-02 08:40:21 +0200

    blank2

--- app/src/main/java/com/bignerddranch/android/criminalintent/SingleListActivity.java
+++ app/src/main/java/com/bignerddranch/android/criminalintent/SingleListActivity.java
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment;
  * Created by hyuntae on 2016-05-16.
  */
 public class SingleListActivity extends SingleFragmentActivity {
-
     @Override
     protected Fragment createFragment(){
         return new CrimeListFragment();
