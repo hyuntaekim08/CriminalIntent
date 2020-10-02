15e859fa86f9f924ae6cdc524e74ac5b961c5a87
Hyuntae Kim <hyuntae.kim@swisscom.com>
2020-10-02 08:39:37 +0200

    blank2

--- app/src/main/java/com/bignerddranch/android/criminalintent/Constants.java
+++ app/src/main/java/com/bignerddranch/android/criminalintent/Constants.java
@@ -2,5 +2,4 @@ package com.bignerddranch.android.criminalintent;
 
 public class Constants {
     public static final String EXTRA_CRIME_ID = "com.bignerddranch.android.criminalintent.crime_id";
-
 }
