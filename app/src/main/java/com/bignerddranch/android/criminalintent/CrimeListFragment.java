20ea653a7e283d164b9cbbb2850b65aefeb68aa1
Hyuntae Kim <hyuntae.kim@swisscom.com>
2020-10-02 08:48:54 +0200

    blank_new_comment1

--- app/src/main/java/com/bignerddranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerddranch/android/criminalintent/CrimeListFragment.java
@@ -36,7 +36,6 @@ public class CrimeListFragment extends ListFragment {
     @Override
     public void onListItemClick(ListView l, View v, int position, long id){
         Crime c = ((CrimeAdapter)getListAdapter()).getItem(position);
-
         Intent i = new Intent(getActivity(), CrimeActivity.class);
         i.putExtra(Constants.EXTRA_CRIME_ID, c.getId());
         startActivity(i);
